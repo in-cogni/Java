088b6a079de995da0c8ed1f72439e4e7b23d6fa7
LadaKashubo <lkashubo@yandex.ru>
2025-11-27 20:34:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    /*System.out.print(10);
+    System.out.println("Age"+19);
+    String A = "str";
+    System.out.println(A);
+    String s = "null"+"hello";
+    String s1 = "hello";
+    int x = 2025;*/
+    
+
+}
