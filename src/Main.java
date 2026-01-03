c19fc8b6619981d1bc2c29a433076c4288be6f27
Incognito <lada.kashubo@yandex.ru>
2026-01-03 21:27:28 +0300

    №9 Record-classes

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,32 @@
 void main() {
     //1
-    Scanner in = new Scanner(System.in);
-    try{
-        System.out.println("Введите число");
-        int a = in.nextInt();
-        if(a==0){
-            throw new ArithmeticException();
-        }
-        float b = 100/a;
-        System.out.println("Результат: "+b);
-    }catch (ArithmeticException | InputMismatchException e){
-        System.out.println("Ошибка "+e.getMessage());
-    }
-    finally{
-        System.out.println("Программа завершила работу");
-        in.close();
-    }
+    User user1 = new User("User1", 8);
+    User user2 = new User("User2", 40);
+    System.out.println(user1.equals(user2));
+    System.out.println(user1);
 
     //2
-    try{
-        int age = parseAge("k");
-        System.out.println("Возраст: "+age);
-    }catch (NumberFormatException e){
+    Product product = new Product("product", 100.0);
+    System.out.println(product);
+
+    try {
+        Product product1 = new Product("", 10.0);
+    } catch (IllegalArgumentException e) {
         System.out.println(e.getMessage());
-    }catch (IllegalArgumentException e){
+    }
+
+    try {
+        Product product2 = new Product("product2", -10.0);
+    } catch (IllegalArgumentException e) {
         System.out.println(e.getMessage());
     }
 
     //3
-    System.out.println("Попытка 1: ");
-    try{
-        BankAccount bankAccount = new BankAccount("", 10);
-        bankAccount.withdraw(1);
-    }catch(Exception e){
-        System.out.println(e.getMessage());
-    }
+    List<OrderItem> list = new ArrayList<>();
+    list.add(new OrderItem("product1", 10, 24.0));
+    list.add(new OrderItem("product2", 4, 40.0));
+    list.add(new OrderItem("product3", 8, 2.0));
 
-    System.out.println("Попытка 2: ");
-    try {
-        BankAccount bankAccount = new BankAccount("User", 1000);
-        bankAccount.withdraw(0);
-    }catch (Exception e){
-        System.out.println(e.getMessage());
-    }
-
-    System.out.println("Попытка 3: ");
-    try {
-        BankAccount bankAccount = new BankAccount("User", 1000);
-        bankAccount.withdraw(1500);
-    }catch (Exception e){
-        System.out.println(e.getMessage());
-    }
-}
-//2
-public static int parseAge(String input) throws NumberFormatException, IllegalArgumentException {
-    int age = Integer.parseInt(input);
-    if (age < 0 | age > 150) {
-        throw new IllegalArgumentException("Возраст не может быть меньше 0 или больше 150");
-    }
-    return age;
+    System.out.println("Общая стоимость: " + list.stream().mapToDouble(OrderItem::totalPrice).sum());
+    System.out.println("Товар с максимальной суммой: "+ list.stream().max(Comparator.comparingDouble(OrderItem::totalPrice)).get());
 }
