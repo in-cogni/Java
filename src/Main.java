623624a101749cd1a558c81a84de5ff1d1babbd2
Incognito <lada.kashubo@yandex.ru>
2026-01-05 22:39:34 +0300

    №11 Stream API

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,32 @@
 void main() {
     //1
-    String text = "This is the text. It is very entertaining. The end.";
-    Map<String, Integer> words = new HashMap<>();
-    String newText = text.toLowerCase(Locale.ROOT);
-    String[] wordsArr = newText.split("[^a-z]+");
-
-    for(String word: wordsArr){
-        if(!word.isEmpty()){
-            words.put(word, words.getOrDefault(word, 0)+1);
-        }
-    }
-
-    Map<String, Integer> sorted = new TreeMap<>(words);
-
-    System.out.println("Уникальные слова: ");
-    for(String name: sorted.keySet()){
-        System.out.print(name+" ");
-    }
-    System.out.println("\nКоличество уникальных слов: "+sorted.size());
-    System.out.println(words);
+    List<Integer> nums = List.of(1, 2, 3, 4, 5, 6);
+    List<Integer> numsStream = nums.stream()
+            .filter(n->n%2!=0)
+            .map(n->n*2)
+            .collect(Collectors.toList());
+    System.out.println(numsStream);//2 6 10
 
     //2
-    Storage<String> storageString = new Storage<>();
-    storageString.add("Petya");
-    storageString.add("Vasya");
-    storageString.add("Dilan");
-    System.out.println(storageString.statistics());
-    System.out.println(storageString.remove("Petya"));
-    System.out.println(storageString.remove("Va"));
-    System.out.println(storageString.statistics());
-    System.out.println(storageString.getHistory());
+    List<String> words = List.of("java", "python", "go", "java", "rust", "go");
+    List<String> wordsStream = words.stream()
+            .distinct()
+            .filter(n->n.length()>3)
+            .sorted()
+            .collect(Collectors.toList());
+    System.out.println(wordsStream);
+
+    //3
+    List<User> users = List.of(
+            new User("Bob", 17),
+            new User("Mila", 22),
+            new User("Ilya", 31),
+            new User("Anna", 17)
+    );
+    Map<Integer, List<User>> usersStream = users.stream()
+            .collect(Collectors.groupingBy(User::age));
+    System.out.println(usersStream);
+
+    //дальше тяжело
 
 }
