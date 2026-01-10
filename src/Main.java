b36ed67cad5b34a80e711660e4b919cc98213bee
Incognito <lada.kashubo@yandex.ru>
2026-01-10 14:11:08 +0300

    15 Optimization IO

--- src/Main.java
+++ src/Main.java
@@ -1,80 +1,32 @@
 void main() {
-    List<String> list = Arrays.asList(
-            "this is a new text",
-            "joe",
-            "0point",
-            "apple strawberry",
-            "0123456789",
-            "01234567890");
-
     //1
-    Path path = Paths.get("input.txt");
-    String filename = "input.txt";
-
-    try{
-        Files.write(path, list, StandardOpenOption.APPEND, StandardOpenOption.CREATE);
-    }catch (IOException e){
-        System.out.println("Error "+e.getMessage());
-    }
-
-    try(BufferedReader reader = new BufferedReader(new FileReader(filename))){
-        String line;
-        int number=1;
-        while((line = reader.readLine())!=null){
-            System.out.printf("%3d: %s%n",number, line);
-            number++;
+    try(FileChannel channel = FileChannel.open(Paths.get("input.txt"), StandardOpenOption.READ)){
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int byteRead = channel.read(buffer);
+        while (byteRead!=-1){
+            buffer.flip();
+            while (buffer.hasRemaining()){
+                System.out.print((char) buffer.get());
+            }
+            buffer.clear();
+            byteRead = channel.read(buffer);
         }
     }catch (IOException e){
-        System.out.println("Error "+e.getMessage());
+        System.out.println(e.getMessage());
     }
 
     //2
-    String filename1 = "source.txt";
-    String filename2 = "result.txt";
-    Path path1 = Paths.get(filename1);
-    Path path2 = Paths.get(filename2);
-    try{
-        Files.write(path1, list, StandardOpenOption.CREATE);
-        if(!Files.exists(path2)){
-            Files.createFile(path2);
+    try(FileChannel src = FileChannel.open(Paths.get("source.bin"), StandardOpenOption.READ);
+        FileChannel dst = FileChannel.open(Paths.get("copy.bin"), StandardOpenOption.CREATE, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING)){
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+        int byteRead = src.read(buffer);
+        while(byteRead!=-1){
+            buffer.flip();
+            dst.write(buffer);
+            buffer.clear();
+            byteRead = src.read(buffer);
         }
     }catch (IOException e){
-        System.out.println("Error "+e.getMessage());
-    }
-
-    try(BufferedReader reader = new BufferedReader(new FileReader(filename1));
-        BufferedWriter writer = new BufferedWriter(new FileWriter(filename2))){
-        String line;
-        while ((line=reader.readLine())!=null){
-            System.out.println("{size: "+line.length()+"} "+line);
-            if(line.length()>10){
-                writer.write(line);
-                writer.newLine();
-            }
-        }
-
-    }catch (IOException e){
-        System.out.println("Error "+e.getMessage());
-    }
-
-    //3
-    String filename3 = "data.bin";
-    String filename4 = "stats.txt";
-    int totalBytes=0;
-    int zeroBytes = 0;
-    try(FileInputStream in = new FileInputStream(filename3);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(filename4))){
-        int bytes;
-        while ((bytes=in.read())!=-1){
-            totalBytes++;
-            if(bytes==0){
-                zeroBytes++;
-            }
-        }
-        writer.write("Total bytes: "+totalBytes);
-        writer.newLine();
-        writer.write("Zero bytes: "+zeroBytes);
-    }catch (IOException e){
-        System.out.println("Error "+e.getMessage());
+        System.out.println(e.getMessage());
     }
 }
